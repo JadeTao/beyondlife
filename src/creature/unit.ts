eafc87c679364bb5c64db6bd51cc906b4a6485f5
jader_tao <jader_tao@qq.com>
2017-08-01 18:20:54 +0800

    fix bug

--- src/creature/unit.ts
+++ src/creature/unit.ts
@@ -1,11 +1,12 @@
-import { state } from './../static/interface';
-import initState from './../static/init-state';
+import { state } from './../static/interface'
+import initState from './../static/init-state'
+
 export default class unit {
   private state: state
   private skill: Array<any>
   constructor(state) {
     //TODO:状态的层次问题
-    this.state = Object.assign({}, initState,{})
+    this.state = Object.assign({}, initState, state)
   }
 }
 console.log(new unit({}))
