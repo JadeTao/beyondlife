51606f20c1ca8dd5d1bde6549b211c1ff1357cf6
jader_tao <jader_tao@qq.com>
2017-08-05 14:46:40 +0800

    format time string output

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,25 @@
+type st = string | number
+function adjust(time: st) {
+  return time < 10 ? "0" + time : time
+}
+
+export default function formatTime(time: number): string {
+  let ret: string
+  if (time < 9) {
+    ret = `[0天 00:00:0${time}]`
+  } else if (time < 60) {
+    ret = `[0天 00:00:${time}]`
+  } else if (time < 3600) {
+    const minute: st = Math.floor(time / 60)
+    const second: st = time % 60
+    // minute = minute < 10 ? "0" + minute : minute
+    // second = second < 10 ? "0" + second : second
+    ret = `[0天 00:${adjust(minute)}:${adjust(second)}]`
+  } else if (time < 86400) {
+    const hour: st = Math.floor(time / 3600)
+    const minute: st = Math.floor((time - 3600 * hour) / 60)
+    const second: st = time % 60
+    ret = `[0天 ${adjust(hour)}:${adjust(minute)}:${adjust(second)}]`
+  }
+  return ret
+}
