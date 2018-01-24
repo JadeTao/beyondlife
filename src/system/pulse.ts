8cc0848fcd9e519780bc7bcd015735972a778d79
jader_tao <jader_tao@qq.com>
2018-01-24 22:17:42 +0800

    update: what a pulse system

---
+++ src/system/pulse.ts
@@ -0,0 +1,26 @@
+import { interval } from 'rxjs/observable/interval'
+import { Observable } from 'rxjs/Rx'
+
+import jade from './../creature/jade'
+import { IArea } from './../static/interface'
+
+export default class Pulse {
+
+    public time
+    public time$
+
+    private source$
+    private realTime
+    private timeHandler: any
+    private sourceHandler
+
+    constructor(private multiple: number = 1) {
+        this.realTime = 0
+        this.time = 0
+        this.source$ = Observable.interval(1000).map((v) => (v + 1) * 1000)
+        this.sourceHandler = this.source$.subscribe((v) => this.realTime = v)
+        const gap = 1000 / multiple;
+        this.time$ = Observable.interval(gap).map((v) => (v + 1) * 1000)
+    }
+
+}
