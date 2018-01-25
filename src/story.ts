5b0bed4692373960b187f0288cce4973bbb71bd4
jader_tao <jader_tao@qq.com>
2018-01-25 22:30:54 +0800

    update: a simple script

---
+++ src/story.ts
@@ -0,0 +1,25 @@
+const SCRIPT = {
+  world: [{
+    name: '斗罗大陆',
+    continent: [{
+      name: '中原',
+    }],
+    hero: {
+      name: 'apex',
+      ability: [{
+        name: 'kiss',
+        duration: [10000],
+        with: true,
+      }],
+    },
+    heroine: {
+      name: 'lingise',
+      ability: [{
+        name: 'sleep',
+        duration: [1000000000000],
+      }],
+    },
+  }],
+}
+
+export default SCRIPT
