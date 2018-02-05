24299ca83aa8cfe4faaa923da8f2d7276ee42bb4
jader_tao <jader_tao@qq.com>
2018-02-05 23:47:50 +0800

    update: enhance abstract base class

--- src/core/role.ts
+++ src/core/role.ts
@@ -4,10 +4,11 @@ import Jade from '../creature/jade'
 import ROLELEVEL from '../static/constant'
 
 export default abstract class Role {
-  public state: any = {}
+
   public children = []
 
   constructor(public pulse, private script: any, private place: any) {
+
   }
 
   abstract interpreter(script: any)
